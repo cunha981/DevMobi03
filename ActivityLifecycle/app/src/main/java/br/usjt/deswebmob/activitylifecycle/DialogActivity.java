af4a69cb86b963486a88739eba70eea4bd4353eb
Igor Almeida <cunha.981@gmail.com>
2018-03-03 19:01:35 -0300

    Activity B e C

--- ActivityLifecycle/app/src/main/java/br/usjt/deswebmob/activitylifecycle/DialogActivity.java
+++ ActivityLifecycle/app/src/main/java/br/usjt/deswebmob/activitylifecycle/DialogActivity.java
@@ -26,6 +26,7 @@ public class DialogActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_dialog);
     }
 
